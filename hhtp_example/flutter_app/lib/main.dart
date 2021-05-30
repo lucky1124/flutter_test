fcbf93da8ba40f4c9a3d962b5a05bdba7c134ef5
lucky1124 <lucky1124@naver.com>
2021-05-30 15:21:44 +0900

    http_ex

--- hhtp_example/flutter_app/lib/main.dart
+++ hhtp_example/flutter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -17,8 +19,16 @@ class HttpApp extends StatefulWidget {
 }
 
 class _HttpAppState extends State<HttpApp> {
-
   String result = "";
+  List data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    data = new List();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +37,58 @@ class _HttpAppState extends State<HttpApp> {
         title: Text('Http Example'),
       ),
       body: Container(
-        child: Text(result),
-      ),
+          child: Center(
+              child: data.length == 0
+                  ? Text(
+                      '데이터가 없습니다',
+                      style: TextStyle(fontSize: 30),
+                      textAlign: TextAlign.center,
+                    )
+                  : ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Text(data[index]['title'].toString()),
+                                Text(data[index]['authors'].toString()),
+                                Text(data[index]['sale_price'].toString()),
+                                Text(data[index]['status'].toString()),
+                                Image.network(
+                                  data[index]['thumbnail'],
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.contain,
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: data.length,
+                    ))),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
+        onPressed: () {
+          getJSONData();
         },
         child: Icon(Icons.file_download),
       ),
     );
   }
-}
 
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var response = await http.get(Uri.parse(url),
+        headers: {'Authorization': "KaKaoAK 10068e5a09bde0cc8d7364d3b2298699"});
+
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+
+      List result = dataConvertedToJSON['documents'];
+
+      data.addAll(result);
+    });
+
+    return response.body;
+  }
+}
